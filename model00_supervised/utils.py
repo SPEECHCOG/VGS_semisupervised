6c15dce43b5db5ac33a85a7e0ee52f6a0e76b2bd
khazarkhorrami <hxkhkh@juovakivikyyhky.lin.tuni.fi>
2022-04-25 15:48:35 +0300

    supervised version created

---
+++ model00_supervised/utils.py
@@ -0,0 +1,98 @@
+import numpy
+import scipy.spatial as ss
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+###############################################################################
+                            # recall #
+###############################################################################
+
+def calculate_recallat10( embedding_1,embedding_2, sampling_times, number_of_all_audios, pool):   
+    recall_all = []
+    recallat = 10  
+    for trial_number in range(sampling_times):      
+        data_ind = numpy.random.randint(0, high=number_of_all_audios, size=pool)       
+        vec_1 = [embedding_1[item] for item in data_ind]
+        vec_2 = [embedding_2[item] for item in data_ind]           
+        distance_utterance = ss.distance.cdist( vec_1 , vec_2 ,  'cosine') # 1-cosine
+       
+        r = 0
+        for n in range(pool):
+            ind_1 = n #random.randrange(0,number_of_audios)                   
+            distance_utterance_n = distance_utterance[n]            
+            sort_index = numpy.argsort(distance_utterance_n)[0:recallat]
+            r += numpy.sum((sort_index==ind_1)*1)   
+        recall_all.append(r)
+        del distance_utterance  
+        
+    return recall_all
+
+
+###############################################################################
+                    # weak supervision ( triplet loss) #
+###############################################################################
+
+def prepare_triplet_data (Ydata, Xdata):    
+    #..........................................................Triplet
+    n_samples = len(Ydata)
+    orderX,orderY = randOrder(n_samples)
+    
+    bin_triplet = numpy.array(make_bin_target(n_samples)) 
+    Ydata_triplet = Ydata[orderY]
+    Xdata_triplet = Xdata[orderX]
+    return Ydata_triplet, Xdata_triplet, bin_triplet
+
+  
+def make_bin_target (n_sample):
+    target = []
+    for group_number in range(n_sample):    
+        target.append(1)
+        target.append(0)
+        target.append(0)
+        
+    return target        
+
+def randOrder(n_t):
+    random_order = numpy.random.permutation(int(n_t))
+    random_order_X = numpy.random.permutation(int(n_t))
+    random_order_Y = numpy.random.permutation(int(n_t))
+    
+    data_orderX = []
+    data_orderY = []     
+    for group_number in random_order:
+        
+        data_orderX.append(group_number)
+        data_orderY.append(group_number)
+        
+        data_orderX.append(group_number)
+        data_orderY.append(random_order_Y[group_number])
+        
+        data_orderX.append(random_order_X[group_number])
+        data_orderY.append(group_number)
+        
+    return data_orderX,data_orderY
+    
+ 
+
+def triplet_loss(y_true,y_pred):    
+    margin = 0.1
+    Sp = y_pred[0::3]
+    Si = y_pred[1::3]
+    Sc = y_pred[2::3]      
+    return K.sum(K.maximum(0.0,(Sc-Sp + margin )) + K.maximum(0.0,(Si-Sp + margin )),  axis=0) 
+
+
+
+###############################################################################
+                    # supervised similarity (bert) #
+############################################################################### 
+
+from similarity_analysis import bert_distance
+
+def find_y_true (pairs, similarity_type):
+    y_true = []
+    for pair in pairs:
+        if similarity_type =="bert":
+            distance_matrix = bert_distance(pair)
+            distance_pair = distance_matrix[0,1]
+            y_true.append(distance_pair)
